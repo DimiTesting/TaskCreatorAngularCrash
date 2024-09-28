3f0403844a27e6d99e95c32a3ec5285ed31e2107
amankeldi.kurban <dimitesting31@gmail.com>
2024-09-28 13:26:39 +0300

    setting up mock tasksJson, and two components Tasks and task-item

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Task } from '../../Task';
+import { TASKS } from '../../mock-tasks';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = TASKS;
+
+  constructor() {}
+
+  ngOnInit(): void {
+      
+  }
+}
