bc1067fe2daabf99bfa3b646f2dae8deeb39be89
amankeldi.kurban <dimitesting31@gmail.com>
2024-09-29 13:26:48 +0300

    fixing up task item

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Task } from '../../Task';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {faTimes} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css'
 })
-export class TaskItemComponent {
+export class TaskItemComponent implements OnInit {
+  @Input() task: Task; 
+  faTimes = faTimes;
+
+  ngOnInit(): void {
+      
+  }
 
 }
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -2,18 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { Task } from '../../Task';
 import { TASKS } from '../../mock-tasks';
+import { TaskItemComponent } from '../task-item/task-item.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, TaskItemComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent implements OnInit {
   tasks: Task[] = TASKS;
-
-  constructor() {}
 
   ngOnInit(): void {
       
