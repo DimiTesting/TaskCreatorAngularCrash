66e11e6334358a54df59b4bab82cd1661f29553d
amankeldi.kurban <dimitesting31@gmail.com>
2024-09-30 17:59:56 +0300

    adding add task functionality

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -14,6 +14,7 @@ import {faTimes} from '@fortawesome/free-solid-svg-icons';
 export class TaskItemComponent implements OnInit {
   @Input() task: Task; 
   @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
   faTimes = faTimes;
 
   ngOnInit(): void {
@@ -22,6 +23,10 @@ export class TaskItemComponent implements OnInit {
 
   onDelete(task: Task) {
     this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);
   }
 
 }
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { Task } from '../../Task';
-import { TaskItemComponent } from '../task-item/task-item.component';
+import { TaskItemComponent} from '../task-item/task-item.component';
+import { AddTaskComponent } from '../add-task/add-task.component';
 import { TaskService } from '../../services/task.service';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [NgFor, TaskItemComponent],
+  imports: [NgFor, TaskItemComponent, AddTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
@@ -30,5 +31,15 @@ export class TasksComponent implements OnInit {
       .subscribe(
         ()=> this.tasks = this.tasks.filter((t) => t.id !== task.id)
       );
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  } 
+
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((task)=> this.tasks.push(task));
+    console.log(task);
   }
 }
